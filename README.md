76ad686785e980f06e3ae09d1a5f38cae7a1d89f
Matthew Ballance <matt.ballance@gmail.com>
2025-12-05 02:31:44 +0000

    Switch to top-of-trunk Verilator builds from master branch

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # verilator-bin
 Binary build of Verilator for various platforms
+
+## Prerequisites
+
+### Ubuntu/Debian
+```bash
+sudo apt-get install -y cmake build-essential autoconf flex bison \
+    help2man python3 python3-venv git
+```
+
+### CentOS/RHEL
+```bash
+sudo yum install -y cmake gcc gcc-c++ autoconf flex bison \
+    help2man python3 git
+```
+
+**Note:** GMP and MPFR are automatically downloaded and built by the CMake build system.
+
+## Building
+
+### Build with tagged releases (default branches):
+```bash
+mkdir build && cd build
+cmake .. -DUSE_LATEST_BRANCH=OFF
+make -j$(nproc)
+```
+
+### Build with latest main/master branches:
+```bash
+mkdir build && cd build
+cmake .. -DUSE_LATEST_BRANCH=ON
+make -j$(nproc)
+```
+
+### Build with specific tags:
+```bash
+mkdir build && cd build
+cmake .. -DVERILATOR_TAG=v5.030 -DBITWUZLA_TAG=0.6.0
+make -j$(nproc)
+```
+
+The built binaries will be in `build/install/`
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,33 @@
 #!/bin/sh -x
 
 root=$(pwd)
-PATH_SAV=${PATH}
-build_rls=false
 
-
+#********************************************************************
+#* Install required packages
+#********************************************************************
 if test $(uname -s) = "Linux"; then
     yum update -y
-    yum install -y glibc-static wget flex bison jq help2man 
+    yum install -y glibc-static wget flex bison jq help2man \
+        cmake3 autoconf make gcc gcc-c++ git
 
     if test -z $image; then
         image=linux
     fi
     export PATH=/opt/python/cp312-cp312/bin:$PATH
+    
+    # Create cmake symlink if cmake3 exists
+    if test -f /usr/bin/cmake3 && test ! -f /usr/bin/cmake; then
+        ln -s /usr/bin/cmake3 /usr/bin/cmake
+    fi
+    
     rls_plat=${image}
 elif test $(uname -s) = "Windows"; then
     rls_plat="windows-x64"
 fi
 
+#********************************************************************
+#* Validate environment variables
+#********************************************************************
 if test -z $vlt_latest_rls; then
   echo "vlt_latest_rls not set"
   env
@@ -30,29 +40,9 @@ if test -z $bwz_latest_rls; then
   exit 1
 fi
 
-if test ! -d py; then
-    python3 -m venv py
-    if test $? -ne 0; then exit 1; fi
-
-    ./py/bin/pip install meson ninja
-    if test $? -ne 0; then exit 1; fi
-fi
-
-if test $build_rls == "true"; then
-    if test ! -f ${vlt_latest_rls}.tar.gz; then
-        wget https://github.com/verilator/verilator/archive/refs/tags/${vlt_latest_rls}.tar.gz
-        if test $? -ne 0; then exit 1; fi
-    fi
-else
-    git clone https://github.com/verilator/verilator.git
-    if test $? -ne 0; then exit 1; fi
-fi
-
-if test ! -f ${bwz_latest_rls}.tar.gz; then
-    wget https://github.com/bitwuzla/bitwuzla/archive/refs/tags/${bwz_latest_rls}.tar.gz
-    if test $? -ne 0; then exit 1; fi
-fi
-
+#********************************************************************
+#* Calculate version information
+#********************************************************************
 if test -z ${rls_version}; then
     vlt_version=$(echo $vlt_latest_rls | sed -e 's/^v//')
     rls_version=${vlt_version}
@@ -62,87 +52,44 @@ if test -z ${rls_version}; then
     fi
 fi
 
-#release_dir="${root}/release/verilator-${rls_version}"
-release_dir="${root}/release/verilator"
-rm -rf ${release_dir}
-mkdir -p ${release_dir}
-
 #********************************************************************
-#* Build Bitwuzla
+#* Build using CMake
 #********************************************************************
 cd ${root}
-bwz_version=${bwz_latest_rls}
-if test -d bitwuzla-${bwz_version}; then
-    rm -rf bitwuzla-${bwz_version}
-fi
 
-tar xvzf ${bwz_latest_rls}.tar.gz
-if test $? -ne 0; then exit 1; fi
-
-export PATH=${root}/py/bin:${PATH}
-
-cd bitwuzla-${bwz_version}
-./configure.py --prefix ${release_dir}
-if test $? -ne 0; then exit 1; fi
-
+# Create build directory
+rm -rf build
+mkdir -p build
 cd build
 
-meson compile
-if test $? -ne 0; then exit 1; fi
-
-meson install
-if test $? -ne 0; then exit 1; fi
-
-# PATH=${PATH_SAV}
-
-#********************************************************************
-#* Build Verilator
-#********************************************************************
-cd ${root}
-
-if test $build_rls == "true"; then
-    if test -d verilator-${vlt_version}; then
-        rm -rf verilator-${vlt_version}
-    fi
- 
-    tar xvzf ${vlt_latest_rls}.tar.gz
-    if test $? -ne 0; then exit 1; fi
-
-    mv verilator-${vlt_version} verilator-src
+# Configure CMake with version information from GitHub workflow
+# When vlt_latest_rls is "master", use USE_LATEST_BRANCH=ON for top-of-trunk builds
+if test "${vlt_latest_rls}" = "master"; then
+  cmake .. \
+    -DUSE_LATEST_BRANCH=ON \
+    -DBITWUZLA_TAG=${bwz_latest_rls} \
+    -DCMAKE_INSTALL_PREFIX=${root}/release/verilator
 else
-    mv verilator verilator-src
+  cmake .. \
+    -DUSE_LATEST_BRANCH=OFF \
+    -DVERILATOR_TAG=${vlt_latest_rls} \
+    -DBITWUZLA_TAG=${bwz_latest_rls} \
+    -DCMAKE_INSTALL_PREFIX=${root}/release/verilator
 fi
 
-cd verilator-src
-autoconf
-if test $? -ne 0; then exit 1; fi
-./configure --prefix=${release_dir} --with-solver='bitwuzla'
 if test $? -ne 0; then exit 1; fi
 
+# Build
 make -j$(nproc)
 if test $? -ne 0; then exit 1; fi
-make install
-if test $? -ne 0; then exit 1; fi
 
 #********************************************************************
-#* Clean-up
-#********************************************************************
-cd ${release_dir}/bin
-strip *
-cd ${release_dir}/share/verilator/bin
-strip *
-rm -rf ${release_dir}/lib
-rm -f ${release_dir}/share/verilator/bin/*_dbg
-
-sed -i ${release_dir}/share/verilator/include/verilated.mk \
-  -e 's%PYTHON3 =.*$%PYTHON3 = python3%g'
-
-#********************************************************************
-#* Clean-up
+#* Create release tarball
 #********************************************************************
 cd ${root}/release
 
-
-#tar czf verilator-linux-${vlt_version}.tar.gz verilator
 tar czf verilator-${rls_plat}-${rls_version}.tar.gz verilator
+if test $? -ne 0; then exit 1; fi
+
+echo "Build complete: verilator-${rls_plat}-${rls_version}.tar.gz"
 
